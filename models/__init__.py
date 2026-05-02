20e416df0b673e57049b1f5c63416e980656d618
Tarno <mateusztarnowski80@gmail.com>
2026-05-02 15:31:46 +0200

    nauka

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+from models.item import ItemModel
+from models.tag import TagModel
+from models.store import StoreModel
+from models.item_tags import ItemsTags
+from models.user import UserModel
+
+
+__all__ = ["ItemModel", "TagModel", "StoreModel", "ItemsTags", "UserModel"] 
---
+++ resources/__init__.py
@@ -0,0 +1,6 @@
+from resources.item import blp as ItemBlueprint
+from resources.store import blp as StoreBlueprint
+from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
+
+__all__ = ["ItemBlueprint", "StoreBlueprint", "TagBlueprint", "UserBlueprint"]
